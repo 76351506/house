56bf0bfa420449e855c4c53862a69c1a8902a7ba
haonan <76351506@163.com>
2023-07-25 19:25:43 +0800

    咨询CURD接口开发

--- house-server/app/controller/infos.js
+++ house-server/app/controller/infos.js
@@ -1,8 +1,8 @@
 /*
  * @Author: heinan
  * @Date: 2023-07-23 22:51:39
- * @Last Modified by: zhiwei
- * @Last Modified time: 2023-07-25 18:54:34
+ * @Last Modified by: heinan
+ * @Last Modified time: 2023-07-25 19:23:36
  */
 "use strict";
 const { Controller } = require("egg");
@@ -14,7 +14,7 @@ class Infos extends Controller {
       this.ctx.body = {
         code: 1,
         msg: "查询成功！",
-        data: result,
+        ...result,
       };
     } else {
       this.ctx.body = {
@@ -24,37 +24,65 @@ class Infos extends Controller {
       };
     }
   }
-  // async destory() {
-  //   const result = await this.ctx.service.infos.destory(this.ctx.params);
-  //   if (result.affectedRows) {
-  //     this.ctx.body = {
-  //       code: 1,
-  //       message: "删除成功！",
-  //     };
-  //   } else {
-  //     this.ctx.body = {
-  //       code: 0,
-  //       message: "删除失败！",
-  //     };
-  //   }
-  // }
+  async destory() {
+    const result = await this.ctx.service.infos.destory(this.ctx.params);
+    if (result.affectedRows) {
+      this.ctx.body = {
+        code: 1,
+        message: "删除成功！",
+      };
+    } else {
+      this.ctx.body = {
+        code: 0,
+        message: "删除失败！",
+      };
+    }
+  }
   async update() {
-    const result = await this.ctx.service.update({
+    const result = await this.ctx.service.infos.update({
       ...this.ctx.params,
       ...this.ctx.request.body,
     });
     if (result.affectedRows) {
-      ctx.body = {
+      this.ctx.body = {
         code: 1,
         message: "修改成功！",
       };
     } else {
-      ctx.body = {
+      this.ctx.body = {
         code: 0,
         message: "修改失败！",
+      };
+    }
+  }
+  async create() {
+    try {
+      this.ctx.validate({
+        uid: "string",
+        title: "string",
+        status: "string",
+      });
+    } catch (err) {
+      this.ctx.status = 406;
+      return (this.ctx.body = {
+        code: 0,
+        error: err.errors,
+      });
+    }
+    const result = await this.ctx.service.infos.create(this.ctx.request.body);
+    if (result.affectedRows) {
+      this.ctx.body = {
+        code: 1,
+        message: "新增成功!",
+      };
+    } else {
+      this.ctx.body = {
+        code: 0,
+        message: "新增失败!",
       };
     }
   }
 }
 
 module.exports = Infos;
+
--- house-server/app/service/infos.js
+++ house-server/app/service/infos.js
@@ -1,9 +1,10 @@
 /*
  * @Author: heinan
- * @Date: 2020-07-16 11:33:19
- * @Last Modified by: zhiwei
- * @Last Modified time: 2023-07-25 19:02:46
+ * @Date: 2023-07-25 19:10:55
+ * @Last Modified by: heinan
+ * @Last Modified time: 2023-07-25 19:21:11
  */
+
 "use strict";
 const { Service } = require("egg");
 const { idCreator } = require("../utils/");
@@ -14,32 +15,44 @@ class InfoService extends Service {
     if (name) {
       where.name = name;
     }
-    if (status == "请选择") {
+    if (status == 0) {
     } else if (status) {
       where.status = status;
     } else {
     }
-    return await this.app.mysql.select("infos", {
+    const result = await this.app.mysql.select("infos", {
       where,
       offset: (currentPage - 1) * pageSize,
       limit: parseInt(pageSize),
     });
+    const total = await this.app.mysql.count("controls", where);
+    return {
+      total,
+      pageSize: Number(pageSize),
+      currentPage: Number(currentPage),
+      data: result,
+    };
   }
   async destory({ id }) {
     return await this.app.mysql.delete("infos", { id });
   }
-  async create({ view_authority_text, view_name }) {
+  async create({ uid, status, title }) {
     const $data = {
-      name,
-      view_name,
-      view_authority_id: idCreator(),
+      uid,
+      status,
+      title,
+      create_time: new Date().getTime(),
+      update_time: new Date().getTime(),
+      id: idCreator(),
     };
     return await this.app.mysql.insert("infos", $data);
   }
-  async update({ id, status }) {
-    const $data = { id, status };
+  async update({ id, uid, status, title }) {
+    const $data = { id, uid, status, title };
+    $data.update_time = new Date().getTime();
     return await this.app.mysql.update("infos", $data);
   }
 }
 
 module.exports = InfoService;
+
